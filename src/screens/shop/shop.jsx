2c987d21d8cfaadd86ca8fa024193f8300a61233
orihero <aka.orihero@gmail.com>
2021-09-15 20:53:25 +0500

    push

--- src/screens/shop/shop.jsx
+++ src/screens/shop/shop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { requests } from "../../api/requests";
+import FeatherIcon from "feather-icons-react";
 import "./shop.css";
 
 export default function Shop() {
@@ -19,6 +20,14 @@ export default function Shop() {
         return (
           <div className="item" key={i}>
             <img src={e.photo_url} />
+            <div className="foods">
+              <p>{e.name}</p>
+              {e.description}
+            </div>
+            <div className="icon">
+              <FeatherIcon icon={"trash-2"} />
+              <FeatherIcon icon={"edit"} />
+            </div>
           </div>
         );
       })}
